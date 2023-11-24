dc8d3189ca599272f4ec1729c94f960a0c043d59
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-24 15:56:35 -0700

    -- First time seeing '\t' in Cpp

---
+++ HotelManagementProject/HotelManagementmainFile.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+int main() {
+
+    int quantity;
+    int choice;
+
+    // Quanity of these items:
+    int QuantRooms = 0, QuantPasta = 0, QuantBurgers = 0, QuantNoodles = 0, QuantShakes = 0, QuantChicken = 0;
+
+    // Sold Number of the items:
+    int SoldRooms = 0, SoldPasta = 0, SoldBurgers = 0, SoldShakes = 0, SoldChicken = 0;
+
+    // Total price for items
+    int Total_rooms = 0, Total_pasta = 0, Total_burger = 0, Total_noodles = 0, Total_shake = 0, Total_chicken = 0;
+
+    std::cout << "\n\t Quantity of items we have";
+    std::cout << "\n Rooms available: ";
+
+
+    return 0;
+}
